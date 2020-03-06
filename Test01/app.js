31b4e258ab17800e75fac069714d0318a55f58c2
luisarendsanchez <luis.arend@spoonds.com>
2020-03-06 12:54:55 +0100

    Update app.js

--- Test01/app.js
+++ Test01/app.js
@@ -19,14 +19,45 @@ con.connect(function(err) {
     //Creates a table with collumns
     /*var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
     con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Table created");
+        if (err) throw err;
+        console.log("Table created");
         });*/
     
     //Alters a table
     /*var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
     con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("Table altered");
-    });*/   
+        if (err) throw err;
+        console.log("Table altered");
+    });*/
+    
+    //Inserts 1 row of data into a table
+    /*var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });*/
+
+    //Inserts multiple rows of data into a table
+    /*var sql = "INSERT INTO customers (name, address) VALUES ?";
+    var values = [
+      ['John', 'Highway 71'],
+      ['Peter', 'Lowstreet 4'],
+      ['Amy', 'Apple st 652'],
+      ['Hannah', 'Mountain 21'],
+      ['Michael', 'Valley 345'],
+      ['Sandy', 'Ocean blvd 2'],
+      ['Betty', 'Green Grass 1'],
+      ['Richard', 'Sky st 331'],
+      ['Susan', 'One way 98'],
+      ['Vicky', 'Yellow Garden 2'],
+      ['Ben', 'Park Lane 38'],
+      ['William', 'Central st 954'],
+      ['Chuck', 'Main Road 989'],
+      ['Viola', 'Sideway 1633']
+    ];
+    con.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });*/
+    
 });
