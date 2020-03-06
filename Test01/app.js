09727c3375eee37c0329e2fa3575e09c4a3f70a6
luisarendsanchez <luis.arend@spoonds.com>
2020-03-06 11:47:36 +0100

    Update app.js

--- Test01/app.js
+++ Test01/app.js
@@ -3,14 +3,30 @@ var mysql = require('mysql2');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root"
+    password: "root",
+    database: "mydb"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-        con.query("CREATE DATABASE mydb", function (err, result) {
+    //Creates a database
+    /*con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });*/
+    
+    //Creates a table with collumns
+    /*var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
             if (err) throw err;
-            console.log("Database created");
-        });
+            console.log("Table created");
+        });*/
+    
+    //Alters a table
+    /*var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+    con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table altered");
+    });*/   
 });
