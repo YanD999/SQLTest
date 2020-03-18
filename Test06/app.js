99bf4bd569cffe6fb4a1ebaeb769e555b99401c1
luisarendsanchez <luis.arend@spoonds.com>
2020-03-18 15:37:53 +0100

    Fixed search results

--- Test06/app.js
+++ Test06/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var mysql = require('mysql');
-var bodyParser  = require("body-parser");
-var app = express();
+let express = require('express');
+let mysql = require('mysql');
+let bodyParser  = require("body-parser");
+let app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/views"));
 
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password: "root",
@@ -16,25 +16,25 @@ var connection = mysql.createConnection({
 
 app.get("/", function(req, res){
     // Find count of users in DB
-    var q = "SELECT * FROM users";
-    var arra = [];
-    var arrb = [];
+    let q = "SELECT * FROM users";
+    let arra = [];
+    let arrb = [];
     connection.query(q, function(err, results, fields){
         if(err) throw err;
-        for (var i = 0; i < results.length; i++) {
-            var a = (results[i].email);
+        for (let i = 0; i < results.length; i++) {
+            let a = (results[i].email);
             arra.push(a);
-            var b = (results[i].created_at);
+            let b = (results[i].created_at);
             arrb.push(b);
         };
-        var emailResult = arra;
-        var createdResult = arrb;
+        let emailResult = arra;
+        let createdResult = arrb;
         res.render("home", {emailResult: emailResult, createdResult:createdResult});
     });
 });
 
 app.post("/register", function(req, res){
-    var person = {
+    let person = {
         email: req.body.email
     };
     connection.query('INSERT INTO users SET ?', person, function(err, result) {
@@ -44,27 +44,27 @@ app.post("/register", function(req, res){
 });
 
 app.post("/search", function(req, res){
-    var user = "%" + req.body.search + "%";
+    let user = "%" + req.body.search + "%";
     let arr = [];
     connection.query('SELECT email FROM users WHERE email like ?', user, function(err, result) {
         if (err) throw err;
-        for (var i = 0; i < result.length; i++) {
-            var a = (result[i].email);
+        for (let i = 0; i < result.length; i++) {
+            let a = (result[i].email);
             arr.push(a);
         }
-        var searchResult = arr;
+        let searchResult = arr;
         res.render("found", {searchResult: searchResult});
     });
 });
 
 app.post("/replace", function(req, res){
-    var replaceFrom = req.body.replaceFrom;
-    var replaceTo = req.body.replaceTo;
+    let replaceFrom = req.body.replaceFrom;
+    let replaceTo = req.body.replaceTo;
     let arr = [];
     connection.query('UPDATE users SET email = ? WHERE email = ?', [replaceTo, replaceFrom], function(err, result) {
         if (err) throw err;
-        for (var i = 0; i < result.length; i++) {
-            var a = (result[i].email);
+        for (let i = 0; i < result.length; i++) {
+            let a = (result[i].email);
             arr.push(a);
         }
         res.redirect("/");
@@ -72,7 +72,7 @@ app.post("/replace", function(req, res){
 });
 
 app.post("/delete", function(req, res){
-    var toDelete = req.body.delete;
+    let toDelete = req.body.delete;
     connection.query('DELETE FROM users WHERE email = ?', toDelete, function(err, result) {
         if (err) throw err;
         res.redirect("/");
@@ -80,9 +80,24 @@ app.post("/delete", function(req, res){
 });
 
 //Doesn't work, To be completed
-app.get("/moreInfo", function(req, res){
-    res.render("show")
-})
+app.post("/moreInfo", function(req, res){
+    let more = req.body.more;
+    let arrc = [];
+    let arrd = [];
+    connection.query('SELECT * FROM users WHERE email like ?', more, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        for (let i = 0; i < result.length; i++) {
+            let a = (result[i].email);
+            arrc.push(a);
+            let b = (result[i].created_at);
+            arrd.push(b);
+        }
+        let infoResult = arrc;
+        let moreCreated = arrd;
+        res.render("show", {infoResult:infoResult, moreCreated:moreCreated});
+    });
+});
 
 app.listen(8080, function(){
     console.log("Server running on 8080!");
